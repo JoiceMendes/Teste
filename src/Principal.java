2f76d1f7f98ad961664eade05c2cb951ee6089d7
Joice Mendes <joice.mendes@gmail.com>
2020-06-18 22:26:35 -0300

    Projeto criado

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Teste");
+		JOptionPane.showMessageDialog(null, "Mesagem");
+
+	}
+
+}
